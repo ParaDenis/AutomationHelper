8f2ab965be384eabcf34618e12be6e139c2d1e9a
Denis Kupets <Denis.Kupets@effective-soft.com>
2017-06-09 09:44:11 +0300

    Added Imap4Client sending

--- AutomationHelper/AutomationHelper/Mailing/Mailer.cs
+++ AutomationHelper/AutomationHelper/Mailing/Mailer.cs
@@ -1,21 +1,29 @@
-﻿using System;
+﻿using ActiveUp.Net.Mail;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ActiveUp.Net.Mail;
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+using Message = ActiveUp.Net.Mail.Message;
 
 namespace AutomationHelper.Mailing
 {
     public class Mailer
     {
         private Imap4Client client;
+
         public Mailer(string server, string address, string password)
         {
-            Client.ConnectSsl(server);
-            Client.Login(address, password);
+            _server = server;
+            _address = address;
+            _password = password;
         }
 
+        private string _server { get; set; }
+        private string _address { get; set; }
+        private string _password { get; set; }
         public IEnumerable<Message> GetAllMails(string mailBox)
         {
             return GetMails(mailBox, "ALL").Cast<Message>();
@@ -31,7 +39,14 @@ namespace AutomationHelper.Mailing
 
         protected Imap4Client Client
         {
-            get { return client ?? (client = new Imap4Client()); }
+            get
+            {
+                if (client != null) return client;
+                client = new Imap4Client();
+                client.ConnectSsl(_server);
+                client.Login(_address, _password);
+                return client;
+            }
         }
 
         public MessageCollection GetMails(string mailBox, string searchPhrase)
@@ -44,6 +59,52 @@ namespace AutomationHelper.Mailing
         public Mailbox GetMailbox(string mailBox)
         {
             return Client.SelectMailbox(mailBox);
+        }
+
+        /// <summary>
+        /// Method can be used to send an e-mail
+        /// </summary>
+        /// <param name="body">The contents of the message</param>
+        /// <param name="address">the e-mail address of the sender</param>
+        /// <param name="password">the password of the sender</param>
+        /// <param name="server">sender's server</param>
+        /// <param name="subject">The subject of the e-mail</param>
+        /// <param name="attachment">Files to be attached</param>
+        /// <param name="emailsSentTo">The emails of the recievers</param>
+        public void SendMailSmtp(string body, string subject,
+            Attachment attachment,
+            string[] emailsSentTo)
+        {
+            var loginInfo = new NetworkCredential(_address, _password);
+            var msg = new MailMessage { From = new MailAddress(_address) };
+            foreach (var s in emailsSentTo)
+                msg.To.Add(new MailAddress(s));
+            msg.Subject = subject;
+            msg.Body = body;
+
+            msg.Attachments.Add(attachment);
+            var smtpClient = new System.Net.Mail.SmtpClient(_server);
+            smtpClient.EnableSsl = true;
+            smtpClient.UseDefaultCredentials = true;
+            smtpClient.Credentials = loginInfo;
+            smtpClient.Send(msg);
+            attachment.Dispose();
+            msg.Dispose();
+            smtpClient.Dispose();
+        }
+        /// <summary>
+        /// Method captures the screenshot and stores it as png file
+        /// </summary>
+        /// <param name="screenshotFileName"></param>
+        public void CaptureScreen(string screenshotFileName)
+        {
+            var bmp = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            var graph = Graphics.FromImage(bmp);
+            graph.CopyFromScreen(0, 0, 0, 0, bmp.Size);
+
+            bmp.Save(screenshotFileName, ImageFormat.Png);
+            bmp.Dispose();
+            graph.Dispose();
         }
     }
 }
