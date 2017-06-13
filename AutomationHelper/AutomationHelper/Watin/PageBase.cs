aab93123ba935a33561fadfce8b76b0bc477ceb7
Denis Kupets <Denis.Kupets@effective-soft.com>
2017-06-13 15:27:09 +0300

    Added Watin

---
+++ AutomationHelper/AutomationHelper/Watin/PageBase.cs
@@ -0,0 +1,18 @@
+﻿using WatiN.Core;
+
+namespace AutomationHelper.Watin
+{
+    public abstract class IEWatinPageBase
+    {
+        public static Browser Browser;
+        protected IEWatinPageBase(string url)
+        {
+            Browser = Browser.AttachTo<IE>(Find.ByUrl(url));
+        }
+
+        public void Close()
+        {
+            Browser.Close();
+        }
+    }
+}
